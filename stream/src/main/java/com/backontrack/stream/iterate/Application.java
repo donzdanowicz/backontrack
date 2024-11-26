f3c4c297c4fcdaf5d24ff36868c6c90a2ceb87c2
donzdanowicz <madpz89@gmail.com>
2024-11-26 13:41:58 +0100

    7.2 First streams. Using of final.

---
+++ stream/src/main/java/com/backontrack/stream/iterate/Application.java
@@ -0,0 +1,8 @@
+package com.backontrack.stream.iterate;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+    }
+}
